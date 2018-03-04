751cb919154c2c0d1b6a30d9bf6498a57f22d930
Kadar Mohamed <kadarmohamed@gmail.com>
2018-03-04 21:12:03 +0100

    cart functions, some jquery

--- web-shop/scripts/checkout.js
+++ web-shop/scripts/checkout.js
@@ -1,4 +1,4 @@
-function validate () {
+function validate() {
     let firstName = document.getElementById("firstname");
     let lastName = document.getElementById("lastname");
     let mail = document.getElementById("mail");
@@ -67,95 +67,125 @@ function validate () {
     return true;
 
 }
+$(document).ready(function() {
 
-/*var test = localStorage.getItem('numberOfItems');
-console.log(localStorage.getItem('name'), localStorage.getItem('quant'));
-document.getElementById("cartItems").innerText = test;*/
+    /*var test = localStorage.getItem('numberOfItems');
+    console.log(localStorage.getItem('name'), localStorage.getItem('quant'));
+    document.getElementById("cartItems").innerText = test;*/
 
 
-var retrievedObject = localStorage.getItem("ids");
-var stored = JSON.parse(retrievedObject);
-var retrievedQ = localStorage.getItem("quantity");
-var storedQ = JSON.parse(retrievedQ);
+    var retrievedNames = sessionStorage.getItem("ids");
+    var storedNames = JSON.parse(retrievedNames);
+    var retrievedQ = sessionStorage.getItem("quantity");
+    var storedQ = JSON.parse(retrievedQ);
 
-console.log(stored);
-console.log(storedQ);
-var testar = {};
-for(var i = 0; i < stored.length; i++) {
-    //console.log(storedQ[i] + storedQ[i + 1]);
-    //testar.push(stored[i], storedQ[i]);
-    if(testar.hasOwnProperty(stored[i])) {
-        testar[stored[i]] = parseInt(testar[stored[i]]) + storedQ[i];
+    console.log(storedNames);
+    console.log(storedQ);
+    var teasInCart = {};
+    for (var i = 0; i < storedNames.length; i++) {
+        //console.log(storedQ[i] + storedQ[i + 1]);
+        //teasInCart.push(storedNames[i], storedQ[i]);
+        if (teasInCart.hasOwnProperty(storedNames[i])) {
+            teasInCart[storedNames[i]] = parseInt(teasInCart[storedNames[i]]) + storedQ[i];
+        }
+        else {
+            teasInCart[storedNames[i]] = storedQ[i];
+        }
+        //teasInCart[storedNames[i]] =  storedQ[i];
+        //console.log(parseInt(teasInCart[storedNames[i]]) + storedQ[i]);
     }
-    else {
-        testar[stored[i]] =  storedQ[i];
+    console.log(teasInCart);
+    var getTeas = localStorage.getItem("storeTeas");
+    var teas = JSON.parse(getTeas);
+    let checkoutSum = document.getElementById("checkout-summary");
+    for (i in teas) {
+        if (teasInCart.hasOwnProperty(teas[i].pName)) {
+            console.log(teas[i].pName);
+            console.log(teasInCart[teas[i].pName]);
+            console.log(parseInt(teasInCart[teas[i].pName] * teas[i].price));
+            //document.getElementById("myImage").setAttribute("src", teas[i].pImage);
+            let cartItem = document.createElement("div");
+            let CIleft = document.createElement("div");
+            let CIimageContainer = document.createElement("div");
+            let CIimage = document.createElement("img");
+            let CInfo = document.createElement("div");
+            let CIRight = document.createElement("div");
+            let CIPrice = document.createElement("div");
+            let CIQuantity = document.createElement("div");
+            let CInput = document.createElement("input");
+            let CIRemove = document.createElement("div");
+
+            CIimage.setAttribute("src", teas[i].pImage);
+            CIimage.setAttribute("width", "108");
+            CInfo.innerHTML = teas[i].pName;
+            CIPrice.innerHTML = parseInt(teasInCart[teas[i].pName] * teas[i].price) + ":-";
+            CInput.setAttribute("value", (teasInCart[teas[i].pName]));
+            CInput.setAttribute("type", "number");
+            CInput.setAttribute("min", "0");
+            CIRemove.innerHTML = '<a href="">X</a>';
+            //CIRemove.setAttribute("id", teas[i].pName);
+
+            cartItem.setAttribute("class", "cart-item");
+            cartItem.setAttribute("id", teas[i].pName.replace(/\s/g, ''));
+            CIleft.setAttribute("class", "ci-left");
+            CIimageContainer.setAttribute("class", "ci-img");
+            CInfo.setAttribute("class", "ci-info");
+            CIRight.setAttribute("class", "ci-right");
+            CIPrice.setAttribute("class", "ci-price");
+            CIQuantity.setAttribute("class", "ci-quantity");
+            CIRemove.setAttribute("class", "ci-remove");
+
+            CIimageContainer.appendChild(CIimage);
+            CIleft.appendChild(CIimageContainer);
+            CIleft.appendChild(CInfo);
+            CIRight.appendChild(CIPrice);
+            CIQuantity.appendChild(CInput);
+            CIRight.appendChild(CIQuantity);
+            CIRight.appendChild(CIRemove);
+            cartItem.appendChild(CIleft);
+            cartItem.appendChild(CIRight);
+            checkoutSum.appendChild(cartItem);
+
+            /*CIRemove.addEventListener("click", function () {
+               let child = document.getElementById(teas[i].pName.replace(/\s/g,''));
+               checkoutSum.removeChild(child);
+               window.reload();
+            });*/
+
+        }
     }
-    //testar[stored[i]] =  storedQ[i];
-    //console.log(parseInt(testar[stored[i]]) + storedQ[i]);
-}
-//delete testar['English Breakfast Tea'];
-console.log(testar);
-var getTeas = localStorage.getItem("storeTeas");
-var teas = JSON.parse(getTeas);
-let checkoutSum = document.getElementById("checkout-summary");
-for (i in teas) {
-    if(testar.hasOwnProperty(teas[i].pName)) {
-        console.log(teas[i].pName);
-        console.log(testar[teas[i].pName]);
-        console.log(parseInt(testar[teas[i].pName] * teas[i].price));
-        //document.getElementById("myImage").setAttribute("src", teas[i].pImage);
-        let cartItem = document.createElement("div");
-        let CIleft = document.createElement("div");
-        let CIimageContainer = document.createElement("div");
-        let CIimage = document.createElement("img");
-        let CInfo = document.createElement("div");
-        let CIRight = document.createElement("div");
-        let CIPrice = document.createElement("div");
-        let CIQuantity = document.createElement("div");
-        let CInput = document.createElement("input");
-        let CIRemove = document.createElement("div");
+    let name;
+    $('.checkout-summary').on('change', 'input', function () {
+       console.log($(this).val());
+       //sessionStorage.clear();
+       //storedQ = [];
+       //storedNames = [];
+       name = $(this).closest('.cart-item').find('.ci-info').text();
+       for (i in teas) {
+           if (teas[i].pName === name) {
+               $(this).closest('.ci-right').find('.ci-price').text(+$(this).val() * teas[i].price + ':-');
+           }
+       }
+       //console.log(name);
+       teasInCart[$(this).closest('.cart-item').find('.ci-info').text()] = +$(this).val();
+       //console.log(teasInCart);
+       /*storedNames.push(name);
+       sessionStorage.setItem('ids', JSON.stringify(storedNames));
+       storedQ.push(+$(this).val());
+       sessionStorage.setItem('quantity', JSON.stringify(storedQ))  ;*/
+    });
+    $('.ci-remove').on('click', function (event) {
+        event.preventDefault();
+        name = $(this).closest('.cart-item').find('.ci-info').text();
+        console.log(name);
+        $(this).closest('.cart-item').remove();
+        delete teasInCart[name];
+        console.log(teasInCart);
 
-        CIimage.setAttribute("src", teas[i].pImage);
-        CIimage.setAttribute("width", "108");
-        CInfo.innerHTML = teas[i].pName;
-        CIPrice.innerHTML = parseInt(testar[teas[i].pName] * teas[i].price) + ":-";
-        CInput.setAttribute("value", (testar[teas[i].pName]));
-        CInput.setAttribute("type", "number");
-        CInput.setAttribute("min", "0");
-        CIRemove.innerHTML = '<a href="">X</a>';
-        CIRemove.setAttribute("id", teas[i].pName);
-
-        cartItem.setAttribute("class", "cart-item");
-        cartItem.setAttribute("id", teas[i].pName.replace(/\s/g,''));
-        CIleft.setAttribute("class", "ci-left");
-        CIimageContainer.setAttribute("class", "ci-img");
-        CInfo.setAttribute("class", "ci-info");
-        CIRight.setAttribute("class", "ci-right");
-        CIPrice.setAttribute("class", "ci-price");
-        CIQuantity.setAttribute("class", "ci-quantity");
-        CIRemove.setAttribute("class", "ci-remove");
-
-        CIimageContainer.appendChild(CIimage);
-        CIleft.appendChild(CIimageContainer);
-        CIleft.appendChild(CInfo);
-        CIRight.appendChild(CIPrice);
-        CIQuantity.appendChild(CInput);
-        CIRight.appendChild(CIQuantity);
-        CIRight.appendChild(CIRemove);
-        cartItem.appendChild(CIleft);
-        cartItem.appendChild(CIRight);
-        checkoutSum.appendChild(cartItem);
-
-        /*CIRemove.addEventListener("click", function () {
-           let child = document.getElementById(teas[i].pName.replace(/\s/g,''));
-           checkoutSum.removeChild(child);
-           window.reload();
-        });*/
-
-    }
-}
-removeBtn = document.getElementById("English Breakfast Tea");
-removeBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    document.getElementById("EnglishBreakfastTea").innerHTML = "";
+    });
+    /*removeBtn = document.getElementById("English Breakfast Tea");
+    removeBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        document.getElementById("EnglishBreakfastTea").innerHTML = "";
+    });*/
 });
--- web-shop/scripts/products.js
+++ web-shop/scripts/products.js
@@ -1,149 +1,173 @@
+$(document).ready(function() {
+    let englishBreakfast = {
+        pName: "English Breakfast Tea",
+        desc: "English breakfast tea is a traditional blend of teas originating from Assam, " +
+        "Ceylon, and Kenya. It is one of the most popular blended teas, common in British tea " +
+        "culture.English breakfast tea is a black tea blend usually described as full-bodied, " +
+        "robust, rich and blended to go well with milk and sugar, in a style traditionally " +
+        "associated with a hearty English breakfast.",
+        price: 79,
+        pImage: "../imgs/english-breakfast-black-tea__76269.1501613205.png"
+    };
+    let earlGrey = {
+        pName: "Earl Grey Tea",
+        desc: "Earl Grey tea is a tea blend which has been flavoured with " +
+        "the addition of oil of bergamot. Bergamot is a variety of orange that is often grown in Italy " +
+        "and France. The rind's fragrant oil is added to black tea to give Earl Grey its signature " +
+        "pungent punch.",
+        price: 89,
+        pImage: "../imgs/Earl_Grey_1280.jpg"
+    };
+    let darjeeling = {
+        pName: "Darjeeling",
+        desc: "Darjeeling tea is a tea from the Darjeeling district in West Bengal, India. It is available in black, " +
+        "green, white and oolong. When properly brewed, it yields a thin-bodied, light-coloured infusion with a " +
+        "floral aroma. The flavour can include a tinge of astringent tannic characteristics and a musky spiciness " +
+        "sometimes described as muscatel",
+        price: 99,
+        pImage: "../imgs/darjeeling.jpg"
+    };
 
-let englishBreakfast = {
-    pName: "English Breakfast Tea",
-    desc: "English breakfast tea is a traditional blend of teas originating from Assam, " +
-    "Ceylon, and Kenya. It is one of the most popular blended teas, common in British tea " +
-    "culture.English breakfast tea is a black tea blend usually described as full-bodied, " +
-    "robust, rich and blended to go well with milk and sugar, in a style traditionally " +
-    "associated with a hearty English breakfast.",
-    price: 79,
-    pImage: "../imgs/english-breakfast-black-tea__76269.1501613205.png"
-};
-let earlGrey = {
-    pName: "Earl Grey Tea",
-    desc: "Earl Grey tea is a tea blend which has been flavoured with " +
-    "the addition of oil of bergamot. Bergamot is a variety of orange that is often grown in Italy " +
-    "and France. The rind's fragrant oil is added to black tea to give Earl Grey its signature " +
-    "pungent punch.",
-    price: 89,
-    pImage: "../imgs/Earl_Grey_1280.jpg"
-};
-let darjeeling = {
-    pName: "Darjeeling",
-    desc: "Darjeeling tea is a tea from the Darjeeling district in West Bengal, India. It is available in black, " +
-    "green, white and oolong. When properly brewed, it yields a thin-bodied, light-coloured infusion with a " +
-    "floral aroma. The flavour can include a tinge of astringent tannic characteristics and a musky spiciness " +
-    "sometimes described as muscatel",
-    price: 99,
-    pImage: "../imgs/darjeeling.jpg"
-};
+    let gunpowder = {
+        pName: "Gunpowder",
+        desc: "is a form of Chinese tea in which each leaf has been rolled into a small round pellet. " +
+        "Its English name comes from its resemblance to grains of gunpowder. This rolling method of " +
+        "shaping tea is most often applied either to dried green tea (the most commonly encountered " +
+        "variety outside China) or oolong tea.",
+        price: 69,
+        pImage: "../imgs/gunpowder.jpg"
+    };
 
-let gunpowder = {
-    pName: "Gunpowder",
-    desc: "is a form of Chinese tea in which each leaf has been rolled into a small round pellet. " +
-    "Its English name comes from its resemblance to grains of gunpowder. This rolling method of " +
-    "shaping tea is most often applied either to dried green tea (the most commonly encountered " +
-    "variety outside China) or oolong tea.",
-    price: 69,
-    pImage: "../imgs/gunpowder.jpg"
-};
+    let genmai = {
+        pName: "Genmai Cha",
+        desc: "also called brown rice green tea, is the Japanese name for green tea combined with " +
+        "roasted brown rice. It is sometimes referred to colloquially as \"popcorn tea\" " +
+        "because a few grains of the rice pop during the roasting process and resemble popcorn.",
+        price: 129,
+        pImage: "../imgs/genmai.jpg"
+    };
+    let jasmine = {
+        pName: "Jasmine Tea",
+        desc: "Tea scented with aroma from jasmine blossoms to make a scented tea. " +
+        "Typically, jasmine tea has green tea as the tea base; however, white tea and black " +
+        "tea are also used. The resulting flavour of jasmine tea is subtly sweet and highly fragrant. " +
+        "It is the most famous scented tea in China",
+        price: 109,
+        pImage: "../imgs/Jasmine-tea.jpg"
+    };
 
-let genmai = {
-    pName: "Genmai Cha",
-    desc: "also called brown rice green tea, is the Japanese name for green tea combined with " +
-    "roasted brown rice. It is sometimes referred to colloquially as \"popcorn tea\" " +
-    "because a few grains of the rice pop during the roasting process and resemble popcorn.",
-    price: 129,
-    pImage: "../imgs/genmai.jpg"
-};
-let jasmine = {
-    pName: "Jasmine Tea",
-    desc: "Tea scented with aroma from jasmine blossoms to make a scented tea. " +
-    "Typically, jasmine tea has green tea as the tea base; however, white tea and black " +
-    "tea are also used. The resulting flavour of jasmine tea is subtly sweet and highly fragrant. " +
-    "It is the most famous scented tea in China",
-    price: 109,
-    pImage: "../imgs/Jasmine-tea.jpg"
-};
+    let strawberry = {
+        pName: "Wild Strawberry",
+        desc: "Fresh strawberries, apple pieces, hibiscus flowers and rose hips create a mellow, almost dessert-like tea. " +
+        "Sweet strawberry and sugared rhubarb aroma, beautiful warm berry color, pleasantly aromatic and not super sweet. " +
+        "A caffeine-free cup of pure delight",
+        price: 150,
+        pImage: "../imgs/wild-strawberry.jpg"
+    };
+    let camomile = {
+        pName: "Chamomile Tea",
+        desc: "Chamomile tea is one of the popular varieties of tea in the market and it has many health benefits and " +
+        "uses. These include its ability to protect the skin, lower stress levels, regulate sleep, and " +
+        "soothe menstrual symptoms. It also boosts the immune system, treats gastrointestinal issues...",
+        price: 99,
+        pImage: "../imgs/chamomil.jpg"
+    };
 
-let strawberry = {
-    pName: "Wild Strawberry",
-    desc: "Fresh strawberries, apple pieces, hibiscus flowers and rose hips create a mellow, almost dessert-like tea. " +
-    "Sweet strawberry and sugared rhubarb aroma, beautiful warm berry color, pleasantly aromatic and not super sweet. " +
-    "A caffeine-free cup of pure delight",
-    price: 150,
-    pImage: "../imgs/wild-strawberry.jpg"
-};
-let camomile = {
-    pName: "Chamomile Tea",
-    desc: "Chamomile tea is one of the popular varieties of tea in the market and it has many health benefits and " +
-    "uses. These include its ability to protect the skin, lower stress levels, regulate sleep, and " +
-    "soothe menstrual symptoms. It also boosts the immune system, treats gastrointestinal issues...",
-    price: 99,
-    pImage: "../imgs/chamomil.jpg"
-};
-
-let teas = [englishBreakfast, earlGrey, darjeeling, gunpowder, genmai, jasmine, strawberry, camomile];
-localStorage.setItem("storeTeas", JSON.stringify(teas));
-
-
-
-let productImage = document.querySelectorAll("img");
-for (let i = 0; i < productImage.length; i++) {
-    productImage[i].setAttribute("src", teas[i].pImage);
-
-}
-let productName = document.querySelectorAll("h4");
-for (let i = 0; i < productName.length; i++) {
-    productName[i].innerHTML = teas[i].pName;
-}
-
-let productPrice = document.querySelectorAll(".price");
-for (let i = 0; i < productPrice.length; i++) {
-    productPrice[i].innerHTML = teas[i].price + ":-";
-}
-
-let productDesc = document.querySelectorAll("p");
-for (let i = 0; i < productDesc.length; i++) {
-    productDesc[i].innerHTML = teas[i].desc;
-}
+    let teas = [englishBreakfast, earlGrey, darjeeling, gunpowder, genmai, jasmine, strawberry, camomile];
+    localStorage.setItem("storeTeas", JSON.stringify(teas));
 
 
-/* Lite onödig kod men tanken var att man skulle hålla reda på antalet varor i korgen för att sedan visa
-   sin beställning vid checkout. Den loopar igenom alla LABELS och sätter for-attribut på dem som motsvarar namnet på
-   just den produkten (fast utan mellanslag), den blir också id för input (QUANTITY). Köp-knapparna får ett id som är namnet för
-   produkten.
- */
 
-let btn = document.querySelectorAll("button");
-let tag = document.getElementsByTagName("LABEL");
-for (let i = 0; i < tag.length; i++) {
-    tag[i].setAttribute("for", teas[i].pName.replace(/\s/g,''));
-    document.getElementsByTagName("INPUT")[i].setAttribute("id", teas[i].pName.replace(/\s/g,''));
-    btn[i].setAttribute("id", teas[i].pName);
-}
+    let productImage = document.querySelectorAll("img");
+    for (let i = 0; i < productImage.length; i++) {
+        productImage[i].setAttribute("src", teas[i].pImage);
 
-/* "n" håller reda på id för QUANTITY, "temp" innehåler värdet av QUANTITY och nollställs efter varje tryck på
-    köp-knapparna. "counter" håller räkningen på temp och uppdaterar värdet i kundkorgen.
- */
+    }
+    let productName = document.querySelectorAll("h4");
+    for (let i = 0; i < productName.length; i++) {
+        productName[i].innerHTML = teas[i].pName;
+    }
+
+    let productPrice = document.querySelectorAll(".price");
+    for (let i = 0; i < productPrice.length; i++) {
+        productPrice[i].innerHTML = teas[i].price + ":-";
+    }
+
+    let productDesc = document.querySelectorAll("p");
+    for (let i = 0; i < productDesc.length; i++) {
+        productDesc[i].innerHTML = teas[i].desc;
+    }
+    let temp = 0;
+    let counter = 0;
+    let storeName = [];
+    let storeQ = [];
+
+    /* StoreName keeps track of the names of teas, temp keeps track of many of a particular tea you put in the cart, it is
+       set to 0 for every new tea.
+       counter keeps track of the overall amount of tea in the cart and is updated with temp, it is also displayed in
+       the cartItem span (x).
+     */
+
+    $('button').on('click', function() {
+        $(this).closest('.item-text').find('h4').css('background-color', 'red');
+        storeName.push($(this).closest('.item-text').find('h4').text());
+        temp = +$(this).closest('.item-text').find('input').val();
+        storeQ.push(temp);
+        counter += temp;
+        $('#cartItems').text(' (' + counter + ') ');
+        sessionStorage.setItem("ids", JSON.stringify(storeName));
+        sessionStorage.setItem("quantity", JSON.stringify(storeQ));
+        //console.log($(this).closest('.item-text').find('h4').text());
+        temp = 0;
+    });
+
+
+    /* Lite onödig kod men tanken var att man skulle hålla reda på antalet varor i korgen för att sedan visa
+       sin beställning vid checkout. Den loopar igenom alla LABELS och sätter for-attribut på dem som motsvarar namnet på
+       just den produkten (fast utan mellanslag), den blir också id för input (QUANTITY). Köp-knapparna får ett id som är namnet för
+       produkten.
+     */
+
+    /*let btn = document.querySelectorAll("button");
+    let tag = document.getElementsByTagName("LABEL");
+    for (let i = 0; i < tag.length; i++) {
+        tag[i].setAttribute("for", teas[i].pName.replace(/\s/g,''));
+        document.getElementsByTagName("INPUT")[i].setAttribute("id", teas[i].pName.replace(/\s/g,''));
+        btn[i].setAttribute("id", teas[i].pName);
+    }*/
+
+    /* "n" håller reda på id för QUANTITY, "temp" innehåler värdet av QUANTITY och nollställs efter varje tryck på
+        köp-knapparna. "counter" håller räkningen på temp och uppdaterar värdet i kundkorgen.
+     */
 //document.getElementById("cartItems").innerText = localStorage.getItem("numberOfItems");
 
 
-let temp = 0;
-let counter = 0;
-let n;
-let storeName = [];
-let storeQ = [];
+    /*let temp = 0;
+    let counter = 0;
+    let n;
+    let storeName = [];
+    let storeQ = [];*/
 
 
-btn.forEach(el => el.addEventListener("click", function () {
+    /*btn.forEach(el => el.addEventListener("click", function () {
 
-    console.log(el.getAttribute("id"));
-    storeName.push(el.getAttribute("id"));
-    n = el.getAttribute("id").replace(/\s/g,'');
-    console.log(n);
-    temp = Number(document.getElementById(n).value);
-    storeQ.push(temp);
-
-
-    counter += temp;
-    console.log(counter);
-    document.getElementById("cartItems").innerText = "(" + counter + ")";
-    localStorage.setItem("ids", JSON.stringify(storeName));
-    localStorage.setItem("quantity", JSON.stringify(storeQ));
-    /*localStorage.setItem(el.getAttribute("id"), temp);
-    localStorage.setItem('quant', temp);*/
-    temp = 0;
-}));
+        console.log(el.getAttribute("id"));
+        storeName.push(el.getAttribute("id"));
+        n = el.getAttribute("id").replace(/\s/g,'');
+        console.log(n);
+        temp = Number(document.getElementById(n).value);
+        storeQ.push(temp);
 
 
+        counter += temp;
+        console.log(counter);
+        document.getElementById("cartItems").innerText = "(" + counter + ")";
+        localStorage.setItem("ids", JSON.stringify(storeName));
+        localStorage.setItem("quantity", JSON.stringify(storeQ));
+        /!*localStorage.setItem(el.getAttribute("id"), temp);
+        localStorage.setItem('quant', temp);*!/
+        temp = 0;
+    }));*/
+
+
+});
